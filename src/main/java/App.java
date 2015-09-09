f15c8d8d293b055c5dd8db8008b8cfb28ecfbe8f
Maggie O'Neill <margaretanneoneill@gmail.com>
2015-09-09 16:16:05 -0700

    edit weapons file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,12 +16,15 @@ public class App {
     staticFileLocation("/public"); // Relative path for images, css, etc.
     String layout = "templates/layout.vtl";
 
+    Person player = new Person("John");
+    player.save();
+
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
 
-      model.put("x", 250);
-      model.put("y", 100);
+      model.put("x", player.getXCoordinate());
+      model.put("y", player.getYCoordinate());
 
       model.put("x-bad1", 50);
       model.put("y-bad1", 100);
@@ -29,11 +32,69 @@ public class App {
       model.put("x-bad2", 300);
       model.put("y-bad2", 50);
 
-      model.put("bad1-text", "You won't get away");
-      model.put("good-guy-text", "Watch me!");
+      model.put("player", player);
+      model.put("player-text", "Hello");
+
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-  }
+    get("/movement", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      String left = request.queryParams("left");
+      String right = request.queryParams("right");
+      String up = request.queryParams("up");
+      String down = request.queryParams("down");
 
+      // Person player = Person.find(Integer.parseInt(request.params("id")));
+
+      if(!(left.equals(null)) && left.length()!= 0) {
+        player.moveLeft();
+      }
+      if(!(right.equals(null)) && right.length()!= 0) {
+        player.moveRight();
+      }
+      if(!(up.equals(null)) && up.length()!= 0) {
+        player.moveUp();
+      }
+      if(!(down.equals(null)) && down.length()!= 0) {
+        player.moveDown();
+      }
+      response.redirect("/" );
+      return null;
+    });
+
+    get("/movement/left", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+
+      player.moveLeft();
+      response.redirect("/" );
+      return null;
+    });
+    get("/movement/right", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+
+      player.moveRight();
+      response.redirect("/" );
+      return null;
+    });
+    get("/movement/up", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+
+      player.moveUp();
+      response.redirect("/" );
+      return null;
+    });
+    get("/movement/down", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+
+      player.moveDown();
+      response.redirect("/" );
+      return null;
+    });
+  }
 }
